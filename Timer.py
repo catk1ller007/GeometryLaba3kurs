132a5ceb83510860171ca8f44ac4c395357b3b3e
catk1ller007 <kylaev11@gmail.com>
2023-10-28 14:32:26 +0300

    fix errors

--- Timer.py
+++ Timer.py
@@ -1,3 +1,31 @@
-def writeFile_time(file_path1, arrpoints, timik):
-    with open(file_path1, 'w') as file:
-        file.write(f"{arrpoints} {timik}")
+from Algorithms import *
+import time
+def timer(points):
+
+    # Таймер засекающий время работы алгоритмов
+    # Засекаем время
+    startTime1 = time.time()
+    outJarvis = JarvisAlgo(points)
+    endTime1 = time.time()
+    timeJarvis = endTime1 - startTime1
+
+    startTime2 = time.time()
+    outGraham = GrahamHull(points)
+    endTime2 = time.time()
+    timeGraham = endTime2 - startTime2
+
+    startTime3 = time.time()
+    outGrahamAndr = GrahamAnd(points)
+    endTime3 = time.time()
+    timeGrahamAndr = endTime3 - startTime3
+
+    startTime4 = time.time()
+    outQuickHull = QuickHull(points)
+    endTime4 = time.time()
+    timeQuickHull = endTime4 - startTime4
+
+    print(f"Время выполнения: \n"
+          f"Джарвис = {timeJarvis}\n"
+          f"Грэхем = {timeGraham}\n"
+          f"Грэхем-Эндрю = {timeGrahamAndr}\n"
+          f"Быстрая оболочка = {timeQuickHull}")
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from forFile import *
 from Algorithms import *
 from DrawAlgo import drawAlgo
 from Test import *
-from Timer import writeFile_time
+from Timer import timer
 
 input_file = "input.txt"
 output_file = "output.txt"
@@ -19,44 +19,7 @@ outGrahamAndr = GrahamAnd(points)
 outQuickHull = QuickHull(points)
 
 
-# Таймер засекающий время работы алгоритмов
-# Засекаем время
-startTime1 = time.time()
-outJarvis = JarvisAlgo(points)
-endTime1 = time.time()
-timeJarvis = endTime1 - startTime1
-
-startTime2 = time.time()
-outGraham = GrahamHull(points)
-endTime2 = time.time()
-timeGraham = endTime2 - startTime2
-
-startTime3 = time.time()
-outGrahamAndr = GrahamAnd(points)
-endTime3 = time.time()
-timeGrahamAndr = endTime3 - startTime3
-
-startTime4 = time.time()
-outQuickHull = QuickHull(points)
-endTime4 = time.time()
-timeQuickHull = endTime4 - startTime4
-
-print(f"Время выполнения: \n"
-      f"Джарвис = {timeJarvis}\n"
-      f"Грэхем = {timeGraham}\n"
-      f"Грэхем-Эндрю = {timeGrahamAndr}\n"
-      f"Быстрая оболочка = {timeQuickHull}")
-
-
-
-writeFile_time("timeJarvis" , len(points), timeJarvis)
-writeFile_time("timeGraham" , len(points), timeGraham)
-writeFile_time("timeGrahamAND" , len(points), timeGrahamAndr)
-writeFile_time("timeQuick" , len(points), timeQuickHull)
-
-
-
-
+timer(points)
 
 # Запись алгоритмов
 writeFile(output_file, outJarvis, outGraham, outGrahamAndr, outQuickHull)
